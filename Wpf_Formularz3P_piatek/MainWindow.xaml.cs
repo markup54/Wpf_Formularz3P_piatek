fefd593790d7fd9c6c4c9d30da9a965b2eaa057b
markup54 <marzenakupka@tlen.pl>
2023-12-01 11:02:57 +0100

    obsluga formularza

--- Wpf_Formularz3P_piatek/MainWindow.xaml.cs
+++ Wpf_Formularz3P_piatek/MainWindow.xaml.cs
@@ -24,5 +24,58 @@ namespace Wpf_Formularz3P_piatek
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Wpisano do pola "+bok_txt.Text,
+                "Informacja wstępna",MessageBoxButton.OK, MessageBoxImage.Information);
+            //odczyt txt jako liczba rzeczywista
+            double bok;
+            if(double.TryParse(bok_txt.Text, out bok))
+            {
+                double pole = Math.Pow(bok, 2);
+                pole_label.Content = pole;
+                double obwod = 4 * bok;
+                obwod_txt.Text = obwod.ToString();
+                kwadrat2.Height = bok;
+                kwadrat2.Width = bok;
+          
+            }
+            else
+            {
+                MessageBox.Show("Należy wpisać liczbę");
+            }
+
+            kwadrat2.Fill = (SolidColorBrush)new BrushConverter()
+                .ConvertFromString(kolor.Text);
+
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //kwadrat3.Height = sliderrrr.Value;
+            //kwadrat3.Width = slider.Value;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(przezroczysty.IsChecked == true) {
+                kwadrat1.Opacity = 0.2;
+                    }
+            else
+            {
+                kwadrat1.Opacity = 1;
+            }
+        }
+
+        private void przezroczysty_Checked(object sender, RoutedEventArgs e)
+        {
+            kwadrat1.Opacity = 0.2;
+        }
+
+        private void przezroczysty_Unchecked(object sender, RoutedEventArgs e)
+        {
+            kwadrat1.Opacity = 1;
+        }
     }
 }
